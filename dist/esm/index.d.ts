2d77da90a0ae436c7fff49366873868a3573c6ae
Paulo Solipa <psolipa@product-league.com>
2025-11-29 17:11:42 +0000

    removed dist

--- dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { AlternateIconsPlugin } from './definitions';
-declare const AlternateIcons: AlternateIconsPlugin;
-export * from './definitions';
-export { AlternateIcons };
--- dist/esm/index.js
+++
@@ -1,7 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-const AlternateIcons = registerPlugin('AlternateIcons', {
-    web: () => import('./web').then((m) => new m.AlternateIconsWeb()),
-});
-export * from './definitions';
-export { AlternateIcons };
-//# sourceMappingURL=index.js.map
